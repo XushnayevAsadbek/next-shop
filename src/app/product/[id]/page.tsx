d3bbf7de2b0688ae076a9d9d53c324ee71fab27d
XushnayevAsadbek <xushnaevasadbek@gmail.com>
2023-10-16 23:23:02 +0500

    bulid commit

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -18,8 +18,8 @@ const ProductDetailPage = async ({params:{id } }:Props ) => {
         return(
             <div className="max-w-5xl mx-auto flex flex-col md:flex-row items-center gap-8 px-4 mt-48 pb-10">
                 <CustomImage product={product}/>
-            <div className="divide-2 ">
-            <div className="space-y-2 pb- 8  ">
+            <div className="divide-2">
+            <div className="space-y-2 pb- 8">
             <h1 className="text -2xl md:text-4xl font-bold">
                 {product.title}
             </h1>
--- src/app/shopping-cart/page.tsx
+++ src/app/shopping-cart/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 
 import CustomImage from "@/components/images";
 import { ProductType } from "@/interface";
@@ -100,7 +100,7 @@ const ShoppingCart = () => {
                                                                 (_, i) => (
                                                                     <StarIconOutline
                                                                         key={i}
-                                                                        className='h-4 w-4 text-yellow-500'
+                                                                        className="h-4 w-4 text-yellow-500"
                                                                     />
                                                                 )
                                                             )}
@@ -131,9 +131,7 @@ const ShoppingCart = () => {
                                                     </p>
                                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="h-5 w-5 cursor-pointer duration-150 hover:text-red-500"
                                                         onClick={() => removeProduct(product.id)}>
-                                                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"
-
-                                                        />
+                                                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
 
                                                     </svg>
                                                 </div>
@@ -178,24 +176,24 @@ const ShoppingCart = () => {
                 </div>
             ) : (
                 <div className="flex h-[calc(100vh-80px)] items-center justify-center p-5 bg-white w-full">
-                <div className="text-center">
-                  <div className="inline-flex rounded-full bg-yellow-100 p-4">
-                    <div className="rounded-full stroke-yellow-600 bg-yellow-200 p-4">
-                      <svg className="w-16 h-16" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M14.0002 9.33337V14M14.0002 18.6667H14.0118M25.6668 14C25.6668 20.4434 20.4435 25.6667 14.0002 25.6667C7.55684 25.6667 2.3335 20.4434 2.3335 14C2.3335 7.55672 7.55684 2.33337 14.0002 2.33337C20.4435 2.33337 25.6668 7.55672 25.6668 14Z" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg>
-                    </div>
-                  </div>
-                  <h1 className="mt-5 text-[36px] font-bold text-slate-800 lg:text-[50px]">
-                    Shopping cart is empty
-                  </h1>
-                  <p className="text-slate-600 mt-5 lg:text-lg">The page you are looking for doesn't exist or <br />has been removed.</p>
+                    <div className="text-center">
+                        <div className="inline-flex rounded-full bg-yellow-100 p-4">
+                            <div className="rounded-full stroke-yellow-600 bg-yellow-200 p-4">
+                                <svg className="w-16 h-16" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M14.0002 9.33337V14M14.0002 18.6667H14.0118M25.6668 14C25.6668 20.4434 20.4435 25.6667 14.0002 25.6667C7.55684 25.6667 2.3335 20.4434 2.3335 14C2.3335 7.55672 7.55684 2.33337 14.0002 2.33337C20.4435 2.33337 25.6668 7.55672 25.6668 14Z" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+                            </div>
+                        </div>
+                        <h1 className="mt-5 text-[36px] font-bold text-slate-800 lg:text-[50px]">
+                            Shopping cart is empty
+                        </h1>
+                        <p className="text-slate-600 mt-5 lg:text-lg">The page you are looking for doesn't exist or <br />has been removed.</p>
 
-                  <Link href={'/products'}>
-               <button className="button bg-blue-600 text-white border-transparent mt-4 hover:border-blue-600 hover:bg-transparent hover:text-black ">
-                    Product    
-                </button>
-               </Link>
+                        <Link href={'/products'}>
+                            <button className="button bg-blue-600 text-white border-transparent mt-4 hover:border-blue-600 hover:bg-transparent hover:text-black">
+                                Product
+                            </button>
+                        </Link>
+                    </div>
                 </div>
-              </div>
             )}
         </>
 
